ffe8d1ab48ae4bbf6c2c93f485633285b4c20f83
Tommy Messbauer <tommy@vast.com>
2013-12-02 13:02:36 -0600

    14: added support for browser:true to the format function in generator so that browser urls are relative and not fully qualified

--- tests/tap/urls.js
+++ tests/tap/urls.js
@@ -34,14 +34,60 @@ _.each(server.apps, function(app, appid) {
 
       var uri = app.plugins.router.url('single', params);
       var url = app.plugins.router.format('single', params);
+
       var gold = {
+        protocol: 'http',
+        host: 'www.foo.com',
+        hostname: 'www.foo.com',
         pathname: '/dog/1234',
-        query: { hidePictures: 'true' }
+        query: { hidePictures: 'true' },
+        search: '?hidePictures=true',
+        path: '/dog/1234?hidePictures=true',
+
       };
-      var goldUrl = '/dog/1234?hidePictures=true';
+      var goldUrl = 'http://www.foo.com/dog/1234?hidePictures=true';
 
       t.equal(uri.pathname, gold.pathname, 'Pathname should match the expected url.');
+      t.equal(uri.path, gold.path, 'Path should match the expected url.');
       t.deepEqual(uri.query, gold.query, 'Query should match expected object.');
+      t.deepEqual(uri.search, gold.search, 'Search should match expected object.');
+      t.deepEqual(uri.host, gold.host, 'Host should match expected object.');
+      t.deepEqual(uri.hostname, gold.hostname, 'Hostname should match expected object.');
+      t.deepEqual(uri.protocol, gold.protocol, 'Protocol should match expected object.');
+      t.equal(url, goldUrl, 'Formatted url should match expected value.');
+      t.end();
+    }); 
+
+  }
+
+  if (app.id === 'cats') {
+    test('Test url generation for browser route', function(t) {
+      var params = {
+        id: 1234
+      };
+
+      var uri = app.plugins.router.url('single', params);
+      var url = app.plugins.router.format('single', params);
+
+      var gold = {
+        protocol: 'http',
+        host: 'www.foo.com',
+        hostname: 'www.foo.com',
+        pathname: '/cat/1234',
+        query: null,
+        search: null,
+        path: '/cat/1234',
+
+      };
+      var goldUrl = '/cat/1234';
+
+      t.equal(uri.pathname, gold.pathname, 'Pathname should match the expected url.');
+      t.equal(uri.path, gold.path, 'Path should match the expected url.');
+      t.deepEqual(uri.query, gold.query, 'Query should match expected object.');
+      t.deepEqual(uri.search, gold.search, 'Search should match expected object.');
+      t.deepEqual(uri.host, gold.host, 'Host should match expected object.');
+      t.deepEqual(uri.hostname, gold.hostname, 'Hostname should match expected object.');
+      t.deepEqual(uri.protocol, gold.protocol, 'Protocol should match expected object.');
       t.equal(url, goldUrl, 'Formatted url should match expected value.');
       t.end();
     }); 

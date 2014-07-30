daae626e82efc73624d3297df807043e71a6c85d
Tommy Messbauer <totally.bro@gmail.com>
2014-07-29 23:54:50 -0500

    migrated from grunt to gulp

--- tasks/browser.js
+++ tasks/browser.js
@@ -1,35 +1,27 @@
 var ControllerFactory = require('../lib/controller_factory.js');
 var fs = require('fs');
-var path = require('path');
 var _ = require('lodash');
+var through = require('through2');
 
-module.exports = function(grunt) {
+module.exports.gulpRouter = function(options) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  _.defaults(options, {
+    router_template: require.resolve('./router_template.js.tpl')
+  });
 
-  grunt.registerTask('mixdown-router', 'Generates client (browser) controller code from mixdown-router.', function() {
-    // Tell Grunt this task is asynchronous.
-    var done = this.async();
+  // ControllerFactory generates the controllers from config.
+  var cf = new ControllerFactory(options);
+  var module_source = fs.readFileSync(options.router_template, 'utf-8');
+  var handlers = [];
+  var stream = through.obj();
 
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      dest: './browser_controllers.js',
-      router_template: require.resolve('./router_template.js.tpl')
-    });
+  // init controllers which crawls and builds manifest
+  cf.init(function(err) {
+    var code;
 
-    // ControllerFactory generates the controllers from config.
-    var cf = new ControllerFactory(options);
-    var module_source = grunt.file.read(options.router_template);
-    var handlers = [];
-
-    // init controllers which crawls and builds manifest
-    cf.init(function(err) {
-      if (err) {
-        grunt.fail.error('Controllers init() failed', err);
-        done(err);
-        return;
-      }
+    if (err) {
+      code = err.stack;
+    } else {
 
       // Iterate and generate router plugin code.
       for (var route_name in cf.controllers) {
@@ -39,10 +31,31 @@ module.exports = function(grunt) {
       }
 
       // format the string
-      var src_router = module_source.replace('/** HANDLERS HERE **/', handlers.join(',\n  '));
+      code = module_source.replace('/** HANDLERS HERE **/', handlers.join(',\n  '));
+    }
 
-      // Write the destination file.
-      grunt.file.write(path.join(options.dest, 'router.js'), src_router);
+    // write and end
+    stream.write(code);
+    stream.end();
+
+  });
+
+  return stream;
+};
+
+module.exports.gulpManifest = function(options) {
+
+  // ControllerFactory generates the controllers from config.
+  var cf = new ControllerFactory(options || {});
+  var stream = through.obj();
+
+  // init controllers which crawls and builds manifest
+  cf.init(function(err) {
+    var code;
+
+    if (err) {
+      code = err.stack;
+    } else {
 
       var manifest = cf.manifest();
       _.each(manifest, function(m) {
@@ -52,12 +65,15 @@ module.exports = function(grunt) {
           }
         });
       });
-      var src_manifest = 'module.exports = ' + JSON.stringify(manifest) + ';';
-      grunt.file.write(path.join(options.dest, 'manifest.js'), src_manifest);
 
-      done();
-    });
+      code = 'module.exports = ' + JSON.stringify(manifest) + ';';
+    }
+
+    // write and end
+    stream.write(code);
+    stream.end();
 
   });
 
+  return stream;
 };
